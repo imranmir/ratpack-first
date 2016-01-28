0cfceeff287805d1edd2c35756c8344388ecfc82
Imran Mir <imran@intelligrape.com>
2016-01-28 17:10:49 +0530

    Added few more examples

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,31 +1,44 @@
-import anaimal.Animal
 import anaimal.AnimalModule
-import anaimal.Zoo
-import handlers.RouterHandler
-import handlers.TestHandler
+import ratpack.config.ConfigData
+import ratpack.config.ConfigDataBuilder
 import ratpack.form.Form
 import ratpack.groovy.template.MarkupTemplateModule
 
+import static groovy.json.JsonOutput.prettyPrint
+import static groovy.json.JsonOutput.toJson
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.jackson.Jackson.json
+
+class SimpleConfig {
+    String message
+}
 
 ratpack {
     bindings {
         module MarkupTemplateModule
         module AnimalModule
+
+        final ConfigData configData = ConfigData.of { ConfigDataBuilder builder ->
+            builder.args(args)
+        }
+        bindInstance(SimpleConfig, configData.get('/simple', SimpleConfig))
     }
 
     handlers {
-        get('testAnimal') { Animal an ->
-            render an.run()
+        get('config') { SimpleConfig config ->
+            render(prettyPrint(toJson(config)))
         }
 
-        get("zoo") { Zoo zoo ->
-            render zoo.run()
+        get('renderGtpl') {
+            render groovyMarkupTemplate("profile.gtpl",
+                    name: request.queryParams.name,
+                    title: "PROFILE",
+                    hobbies: ['flying', 'skieing', 'swimming', 'reading'])
         }
 
-        get("persons/:id") {
-            render "I am person with id $pathTokens.id and your name is " + request.queryParams.name
+        get('renderjson') {
+            render json([name: 'imran'])
         }
 
         post("persons") {
@@ -36,17 +49,7 @@ ratpack {
                 render s
             }
         }
-        put("zoo/:id") {
-            String s = ""
-            parse(Form).map { Form f ->
-                s += f.name
-            }.then {
-                render s
-            }
-        }
 
-        get('first', new TestHandler())
 
-        get('router', new RouterHandler())
     }
 }
